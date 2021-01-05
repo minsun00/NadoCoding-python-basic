4b4ccf55bf3e2d08d8da288061d50eb88ac98522
minsun00 <75118893+minsun00@users.noreply.github.com>
2021-01-05 21:25:41 +0900

    lecture0.0~3.5

---
+++ practice.py
@@ -0,0 +1,14 @@
+from random import*
+# 퀴즈1 챕터2
+
+station = "사당"
+print(station+"행 열차가 들어오고 있습니다.")
+station = "신도림"
+print(station+"행 열차가 들어오고 있습니다.")
+station = "인천공항"
+print(station+"행 열차가 들어오고 있습니다.")
+# 퀴즈2  챕터3
+
+oflineDay = randint(4, 28)
+
+print("오프라인 스터디 모임 날짜는 매월" + str(oflineDay) + "일로 선정되었습니다.")
