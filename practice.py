25f367ef4bd09e066d74f0f1f366d0c1daace0f1
minsun00 <75118893+minsun00@users.noreply.github.com>
2021-01-08 23:30:39 +0900

    chapter6

--- practice.py
+++ practice.py
@@ -55,7 +55,7 @@
 # print(subway)
 
 # # 지하철에서 있는 사람을 한 명씩 뒤에서 꺼냄
-# print(subway.pop())  # .pop 맨뒤에값 뻄
+# print(subway.pop())  # .pop() 아무것도 안 넣을 시 맨 뒤에 값 뺌
 # print(subway)
 
 # # 같은 이름의 사람이 몇 명 있는지 확인
@@ -186,3 +186,78 @@
 
 # menu = set(menu)
 # print(menu, type(menu))
+
+# # --------------------------------------------------------챕터 6-1 if----------------------------------------------------------------
+# weather = input("오늘 날씨는 어떄요?") #input은 항상 string 값으로 반환함
+# if weather=="비" or weather=="눈":
+#     print("우산을 챙기세요")
+# elif weather =="미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요.")
+
+# temp = int(input("기온은 어때요?"))
+# if 30<= temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10<=temp and temp <30:
+#     print("괜찮은 날씨에요")
+# elif 0<=temp <10:
+#     print("너무 추워요. 나가지 마세요")
+
+# # --------------------------------------------------------챕터 6-2 for 반복문1--------------------------------------------------------------
+# for Wating_no in range(1, 6):  # 1, 2, 3, 4, 5
+#     print("대기번호:{0}".format(Wating_no))
+
+# starbucks = ["아이언맨", "토르", "그루트"]
+# for customer in starbucks:
+#     print("{0},커피가 준비되었습니다.".format(customer))
+# # --------------------------------------------------------챕터 6-3 while 반복문2----------------------------------------------------------------
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0},커피가 준비 되었습니다.{1} 번 남았어요.".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 폐기처분되었습니다")
+
+# 무한반복 컨트롤 c 누르면 멈춤
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0},커피가 준비 되었습니다.{1} 번 남았어요.".format(customer, index))
+#     index+=1
+
+# customer = "토르"
+# person = "unknown"
+
+# while person != customer:
+#     print("{0},커피가 준비 되었습니다.".format(customer))
+#     person = input("이름이 어떻게 되세요?")
+
+# # --------------------------------------------------------챕터 6-4 continue와 break----------------------------------------------------------------
+# absent = [2, 5]  # 결석
+# no_book = {7}  # 책을 깜빡함
+# for student in range(1, 11):
+#     if student in absent:
+#         continue
+#     elif student in no_book:
+#         print("오늘 수업 여기까지.{0}는 교무실로 따라와".format(student))
+#         break
+#     print("{0},책을 읽어봐".format(student))
+
+# # --------------------------------------------------------챕터 6-5 한줄 for문----------------------------------------------------------------
+# # 출석번호가 1 2 3 4, 앞에 100을 붙이기로 함 -> 101, 102 ,103 ,104.
+# students = [1, 2, 3, 4, 5]
+# print(students)
+# students = [i+100 for i in students]
+# print(students)
+
+# # 학생 이름을 길이로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [len(i)for i in students]
+# print(students)
+
+# # 학생 이름을 대문자로 반환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [i.upper() for i in students]
+# print(students)
--- quiz.py
+++ quiz.py
@@ -1,43 +1,64 @@
 from random import*
-# 퀴즈1 챕터2
+# # 퀴즈1 챕터2
 
-station = "사당"
-print(station+"행 열차가 들어오고 있습니다.")
-station = "신도림"
-print(station+"행 열차가 들어오고 있습니다.")
-station = "인천공항"
-print(station+"행 열차가 들어오고 있습니다.")
-# 퀴즈2  챕터3
+# station = "사당"
+# print(station+"행 열차가 들어오고 있습니다.")
+# station = "신도림"
+# print(station+"행 열차가 들어오고 있습니다.")
+# station = "인천공항"
+# print(station+"행 열차가 들어오고 있습니다.")
+# # 퀴즈2  챕터3
 
-oflineDay = randint(4, 28)
+# oflineDay = randint(4, 28)
 
-print("오프라인 스터디 모임 날짜는 매월" + str(oflineDay) + "일로 선정되었습니다.")
+# print("오프라인 스터디 모임 날짜는 매월" + str(oflineDay) + "일로 선정되었습니다.")
 
-# 퀴즈 3 챕터4
+# # 퀴즈 3 챕터4
 
-website = "http://naver.com"
-webAbbreviation = website[7:-4]
-web3letters = webAbbreviation[:3]
-lenWesite = len(webAbbreviation)
-countE = webAbbreviation.count("n")
+# website = "http://naver.com"
+# webAbbreviation = website[7:-4]
+# web3letters = webAbbreviation[:3]
+# lenWesite = len(webAbbreviation)
+# countE = webAbbreviation.count("n")
 
-password = web3letters+str(lenWesite)+str(countE)+"!"
+# password = web3letters+str(lenWesite)+str(countE)+"!"
 
-print(password)
+# print(password)
 
-# 퀴즈 4 챕터 5
-# 내 답안
-id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-           11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-shuffle(id_list)
-chicken = set(sample(id_list, 1))
-id_list = set(id_list)
-id_list = id_list-chicken
-id_list = list(id_list)
-shuffle(id_list)
-coffee = sample(id_list, 3)
-print(f"-- 당첨자 발표 --\n 치킨 당첨자:{chicken} \n 커피 당첨자:{coffee} \n -- 축하합니다 --")
+# # 퀴즈 4 챕터 5
+# # 내 답안
+# id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+#            11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# shuffle(id_list)
+# chicken = set(sample(id_list, 1))
+# id_list = set(id_list)
+# id_list = id_list-chicken
+# id_list = list(id_list)
+# shuffle(id_list)
+# coffee = sample(id_list, 3)
+# print(f"-- 당첨자 발표 --\n 치킨 당첨자:{chicken} \n 커피 당첨자:{coffee} \n -- 축하합니다 --")
 
-# 나도 코딩님 답안
-users = range(1, 21)  # 1부터 20까지 숫자를 생성
-users = list(users)
+# # 나도 코딩님 답안
+# users = range(1, 21)  # 1부터 20까지 숫자를 생성
+# users = list(users)
+
+# shuffle(users)
+
+# winners = sample(users, 4)  # 4명 중에서 1명은 치킨,3명은 커피
+
+# print("-- 당첨자 발표 --")
+# print("치킨 당첨자 :{0}".format(winners[0]))
+# print("커피 당첨자 : {0}".format(winners[1:]))
+# print("-- 축하합니다 -- ")
+
+# 퀴즈 5 챕터 6
+i = 0
+customers = [[i, randrange(5, 51)] for i in range(1, 51)]
+
+for x in customers:
+    if 5 <= x[1] <= 15:
+        print("[0] {0}번째 손님 (소요시간 : {1}분)".format(x[0], x[1]))
+        i += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(x[0], x[1]))
+print("총 탑승 승객 :{0}".format(i))
